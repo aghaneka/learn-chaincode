0c3e373bcdf3f065b26b103512d4d055f7a1c082
aghaneka <aghaneka@in.ibm.com>
2017-01-28 14:26:06 +0530

    test3

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -91,7 +91,7 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 	
 	s := strings.Join([]byte(value), "Amol Ghanekar")
 	
-	err = stub.PutState(key, s) //write the variable into the chaincode state
+	err = stub.PutState(key, []byte(s)) //write the variable into the chaincode state
 	if err != nil {
 		return nil, err
 	}
