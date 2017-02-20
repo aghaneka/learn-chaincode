edfdc972c5861f684b39a20bcb689464b743da84
aghaneka <aghaneka@in.ibm.com>
2017-02-20 19:14:32 +0530

    ok5

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -14,13 +14,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package finished
+package main
 
 import (
 	"errors"
 	"fmt"
 	"strings"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"finished"
 )
 
 // SimpleChaincode example simple Chaincode implementation
