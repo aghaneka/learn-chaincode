686b8585506622eb5d468bedc32d6ae3048e55cd
aghaneka <aghaneka@in.ibm.com>
2017-02-21 12:01:20 +0530

    ok

--- finished/chaincode_var.go
+++ finished/chaincode_var.go
@@ -13,11 +13,15 @@ func check(e error) {
 }
 
 func getName() (string){
+	
+	return "Geoff Boycott"
+	/*
 	pwd, _ := os.Getwd()
 	fmt.Printf("dir : "+pwd)
 	dat, err := ioutil.ReadFile(pwd+"/writer.dat")
     check(err)
     return string(dat)
+    */
 }
 
 
