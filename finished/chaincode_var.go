5580b1e6b6104d97de172a941845fd8733871edc
aghaneka <aghaneka@in.ibm.com>
2017-02-22 14:26:32 +0530

    ok

--- finished/chaincode_var.go
+++ finished/chaincode_var.go
@@ -1,30 +1,5 @@
-package main
+package finished
 
-import (
-    "net/http"
-//    "fmt"
-    "io/ioutil"
-)
-
-func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+func getName() {
+    return "Tavis bones"
 }
-
-//func main(){
-//	fmt.Printf("Name : "+getName())
-//}
-
-func getName() (string){
-	resp, err := http.Get("http://52.220.79.230:8080/write.html")
-	if err != nil {
-		// handle error
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-    return string(body)
-    
-}
-
-
